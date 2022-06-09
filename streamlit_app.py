8aa8c35e705a2a57898e8ab38a60e560d012a4b2
John-Browning <107151576+John-Browning@users.noreply.github.com>
2022-06-09 17:38:17 -0400

    Update streamlit_app.py
    indentation error

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ try:
     fruityvice_normalized  = pandas.json_normalize(fruityvice_response.json())
 # output to the screen as a table
     streamlit.dataframe(fruityvice_normalized)
- except URLError as e:
+except URLError as e:
   stramlit.error()
 
 streamlit.stop()
