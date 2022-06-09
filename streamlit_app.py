eedafffb07ea8f2b121c27effc8b76bc2cc5b6f9
John-Browning <107151576+John-Browning@users.noreply.github.com>
2022-06-09 18:16:00 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,7 +78,7 @@ def insert_row_snowflake(new_fruit):
     my_cur.execute("insert into fruit_load_list values ('" + new_fruit + "')")
     return "Thanks for adding " + new_fruit
 
-add_my_fruit = streamlit.text_input('What fruit would you like add?', 'jackfruit')
+add_my_fruit = streamlit.text_input('What fruit would you like add?')
 function_result = insert_row_snowflake(add_my_fruit)
 streamlit.write(function_result)
 
